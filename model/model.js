a62014134467503603fc8ffe8f03b21dc6eb45a8
oregan1 <txmxregan@gmail.com>
2022-07-06 12:11:59 +0100

    removed uselss condition

--- model/model.js
+++ model/model.js
@@ -61,7 +61,7 @@ exports.selectArticlesComments = async (id) => {
     const articles = await db.query(`SELECT * FROM articles WHERE article_id = $1`,[id])
     return db.query(`SELECT * FROM comments WHERE article_id = $1`,[id])
     .then((result) => {
-        if (!result.rows[0] && articles.rows.length === 0){
+        if (articles.rows.length === 0){
             return Promise.reject({
                 status: 404,
                 msg: 'No article found with that id'
